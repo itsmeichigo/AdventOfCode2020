86321f855251cab3531600fb0b42e8afaa7339e7
Huong Do <huongdt29@gmail.com>
2020-12-08 16:41:46 +0700

    Replace fast & slow method with simpler solution

--- AdventOfCode2020/Day8/day8.py
+++ AdventOfCode2020/Day8/day8.py
@@ -1,19 +1,17 @@
 import re
 
 def calulate_loop_acc(steps):
-    left, left_acc = 0, 0
-    right, right_acc = find_loop_start(steps)
-    while True:
-        left, left_acc = move_next(steps, left, left_acc)
-        right, right_acc = move_next(steps, right, right_acc)
-        if left == right:
-            break
-    return right_acc
+    current_index, acc = 0, 0
+    visited = []
+    while current_index not in visited and current_index is not None:
+        visited.append(current_index)
+        current_index, acc = move_next(steps, current_index, acc)
+    return (current_index, acc)
 
 def fix_program(steps):
     for index, step in enumerate(steps):
         if step[0] != "acc":
-            start, acc = find_loop_start(switch_op(steps, index))
+            start, acc = calulate_loop_acc(switch_op(steps, index))
             if start is None:
                 return acc
     return -1
@@ -23,20 +21,6 @@ def switch_op(steps, index):
     new_op = "jmp" if steps[index][0] == "nop" else "nop"
     copy[index] = (new_op, steps[index][1])
     return copy
-
-def find_loop_start(steps):
-    slow, fast, slow_acc, fast_acc = 0, 0, 0, 0
-    while True and fast is not None:
-        slow, slow_acc = move_next(steps, slow, slow_acc)
-        fast, fast_acc = move_next(steps, fast, fast_acc)
-        if fast is None:
-            break
-        fast, fast_acc = move_next(steps, fast, fast_acc)
-        if slow == fast:
-            break
-    if fast is None:
-        return (None, fast_acc)
-    return (slow, slow_acc)
 
 def move_next(steps, current_index, acc):
     if current_index >= len(steps):
@@ -55,5 +39,5 @@ steps = []
 with open("data.txt") as file:
     for x in file.readlines():
         steps.extend(re.findall(r"(.{3}) ([+-]\d+)", x))
-    print(calulate_loop_acc(steps))
+    print(calulate_loop_acc(steps)[1])
     print(fix_program(steps))
