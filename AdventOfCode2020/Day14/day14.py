af4249b0115ea23f810d2356fefb1374f73a79ff
Huong Do <huongdt29@gmail.com>
2020-12-14 19:50:42 +0700

    Day 14 part 2

--- AdventOfCode2020/Day14/day14.py
+++ AdventOfCode2020/Day14/day14.py
@@ -1,29 +1,61 @@
 import re
 
-def find_sum_values(rules):
+def find_sum_values_v1(rules):
     memory_map = {}
     for r in rules:
-        if r[0:4] != "mask": r = "mask" + r
-        mask = re.match(r"mask = ([X01]{36})", r).group(1)
-        for i in re.findall(r"mem\[(\d+)\] = (\d+)", r):
-            bin_str = bin(int(i[1])).replace("0b", "")
-            updated = ["0"]*(len(mask) - len(bin_str)) + list(bin_str)
+        mask, mem_info = parse_rule(r)
+        for m in mem_info:
+            updated = list(format(int(m[1]), "036b"))
             for j, char in enumerate(mask):
                 if char != "X": updated[j] = char
-            memory_map[i[0]] = int("".join(updated), 2)
+            memory_map[m[0]] = int("".join(updated), 2)
     return sum(v for v in memory_map.values())
 
+def find_sum_values_v2(rules):
+    memory_map = {}
+    for r in rules: 
+        mask, mem_info = parse_rule(r)
+        options = [format(x, "0{}b".format(mask.count("X"))) for x in range(pow(2, mask.count("X")))]
+        for m in mem_info:
+            raw = list(format(int(m[0]), "036b"))
+            for j, char in enumerate(mask):
+                if char != "0": raw[j] = char
+                
+            for o in options:
+                o_chars = list(o)
+                copy = raw.copy()
+                for k, char in enumerate(copy):
+                    if char == "X": 
+                        copy[k] = o_chars[0]
+                        o_chars = o_chars[1:]
+                memory_map[int("".join(copy), 2)] = int(m[1])
+    return sum(v for v in memory_map.values())
+        
+def parse_rule(r):
+    if r[0:4] != "mask": r = "mask" + r
+    mask = re.match(r"mask = ([X01]{36})", r).group(1)
+    mem_info = re.findall(r"mem\[(\d+)\] = (\d+)", r)
+    return mask, mem_info
+
 def test_input_data():
-    input = """mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+    input1 = """mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
 mem[8] = 0"""
-    assert(find_sum_values([input])) == 165
+    assert(find_sum_values_v1([input1])) == 165
+
+    input2 = """mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1"""
+    rules = re.split(r"\nmask", input2)
+    assert(find_sum_values_v2(rules)) == 208
 
 if __name__ == "__main__":
-    # test_input_data()
+    test_input_data()
     with open("data.txt") as file:
         rules = re.split(r"\nmask", file.read())
-        print(find_sum_values(rules))
+        print(find_sum_values_v1(rules))
+        print(find_sum_values_v2(rules))
     
     
