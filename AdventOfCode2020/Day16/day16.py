27b245152e46090033406fcdffc3c5a804a5bfbc
Huong Do <huongdt29@gmail.com>
2020-12-16 17:52:59 +0700

    Update parsing for part 2

--- AdventOfCode2020/Day16/day16.py
+++ AdventOfCode2020/Day16/day16.py
@@ -1,21 +1,24 @@
 import re
 
-def check_nearby_ticket_error_rate(tickets, ranges):
+def check_nearby_tickets_error(tickets, ranges):
     error_rate = 0
     for t in tickets:
         for num in t:
-            if check_invalid_num(num, ranges):
+            if len(check_valid_fields(num, ranges)) == 0:
                 error_rate += num
                 break
-    
     return error_rate
 
-def check_invalid_num(num, ranges):
-    invalid_count = 0
-    for r in ranges:
-        if num < r[0] or num > r[3] or (num > r[1] and num < r[2]):
-            invalid_count += 1
-    return invalid_count == len(ranges)
+def decode_fields(tickets, ranges):
+    
+    return []
+
+def check_valid_fields(num, ranges):
+    valid = []
+    for k, v in ranges.items():
+        if (num >= v[0] and num <= v[1]) or (num >= v[2] and num <= v[3]):
+            valid.append(k)
+    return valid
 
 def test_input():
     test1 = """class: 1-3 or 5-7
@@ -31,20 +34,36 @@ nearby tickets:
 55,2,20
 38,6,12"""
     ranges, mine, tickets = parse_input(test1)
-    assert(check_nearby_ticket_error_rate(tickets, ranges)) == 71
+    assert(check_nearby_tickets_error(tickets, ranges)) == 71
+
+    test2 = """class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9"""
+    ranges, mine, tickets = parse_input(test2)
+    fields = decode_fields(tickets, ranges)
+    # assert(decode_fields(tickets, ranges)) == ["row", "class", "seat"]
 
 def parse_input(input):
     rules, my_ticket, nearby = input.split("\n\n")
-    ranges = [parse_range(r) for r in rules.splitlines()]
+    ranges = {}
+    for r in rules.splitlines():
+        g = re.match(r"(.+): (\d+)-(\d+) or (\d+)-(\d+)", r)
+        ranges[g.group(1)] = [int(i) for i in g.group(2, 3, 4, 5)]
     mine = [int(i) for i in my_ticket.splitlines()[-1].split(",")]
     tickets = [[int(i) for i in l.split(",")] for l in nearby.splitlines()[1:]]
     return ranges, mine, tickets
-
-def parse_range(r):
-    return [int(i) for i in re.match(r".+: (\d+)-(\d+) or (\d+)-(\d+)", r).group(1, 2, 3, 4)]
 
 if __name__ == "__main__":
     test_input()
     with open("data.txt") as file:
         ranges, mine, tickets = parse_input(file.read())
-        print(check_nearby_ticket_error_rate(tickets, ranges))
+        print(check_nearby_tickets_error(tickets, ranges))
+        # fields = decode_fields(tickets, ranges)
