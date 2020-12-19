c67d1f71069fb86cb57cb663507bf9e15646010a
Huong Do <huongdt29@gmail.com>
2020-12-19 15:35:23 +0700

    Part 2 day 17

--- AdventOfCode2020/Day17/day17.py
+++ AdventOfCode2020/Day17/day17.py
@@ -1,48 +1,51 @@
-def find_neighbors(cube):
+from itertools import product
+from operator import add
+
+def find_neighbors(cube, dimension):
     neighbors = []
-    for a in [-1,1,0]:
-        for b in [-1,1,0]:
-            for c in [-1,1,0]:
-                if a == 0 and b == 0 and c == 0: continue
-                neighbors.append([cube[0]+a, cube[1]+b, cube[2]+c])
+    for step in product([-1,0,1], repeat=dimension):
+        if len([i for i in step if i == 0]) == dimension: continue
+        neighbors.append(tuple(map(add, step, cube)))
     return neighbors
 
-def parse_initial_cubes(input):
-    cubes, z, y = [], 0, len(input.splitlines()) - 1
+def parse_initial_cubes(input, dimension):
+    state, y = {}, len(input.splitlines()) - 1
     for i, line in enumerate(input.splitlines()):
         for j, char in enumerate(line):
-            if char == "#": 
-                cubes.append([j, y - i, z])
-    return cubes
+            value = [j, y - i] + [0] * (dimension - 2)
+            state[tuple(value)] = char
+    return state
 
-def process_cycle(active):
-    result = []
-    for cube in active:
-        neighbors = find_neighbors(cube)
-        active_neighbors = [c for c in neighbors if c in active]
-        nonactive_neighbors = [c for c in neighbors if c not in active]
-        if len(active_neighbors) == 2 or len(active_neighbors) == 3:
-            result.append(cube)
-        for cube in nonactive_neighbors:
-            if len([c for c in find_neighbors(cube) if c in active]) == 3 and cube not in result:
-                result.append(cube)
-    return result
-
-def count_active(input, count):
-    active = parse_initial_cubes(input)
-    i = 0
+def process_cycle(state, dimension):
+    new_state = {}
+    for k in list(state): # expand space to check later
+        for n in find_neighbors(list(k), dimension):
+            if tuple(n) not in state: state[tuple(n)] = "."
+    for k, v in state.items():
+        active = 0
+        for n in find_neighbors(list(k), dimension):
+            if state.get(tuple(n), ".") == "#": active += 1
+        if v == "#" and active in [2,3]: new_state[k] = "#"
+        elif v == "." and active == 3: new_state[k] = "#"
+        else: new_state[k] = "."
+    return new_state
+        
+def count_active(input, count, dimension):
+    state, i = parse_initial_cubes(input, dimension), 0
     while i < count:
-        active = process_cycle(active)
+        state = process_cycle(state, dimension)
         i += 1
-    return len(active)
+    return len([k for k, v in state.items() if v == "#"])
 
 def test_input():
     input = """.#.
 ..#
 ###"""
-    assert(count_active(input, 6)) == 112
+    assert(count_active(input, 6, 3)) == 112
+    assert(count_active(input, 6, 4)) == 848
 
 if __name__ == "__main__":
     test_input()
     with open("data.txt") as file:
-        print(count_active(file.read(), 6))
+        print(count_active(file.read(), 6, 3))
+        print(count_active(file.read(), 6, 4))
