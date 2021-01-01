4881a0e0abb1cbfc2d32897c6d8c90461f1ed10b
Huong Do <huongdt29@gmail.com>
2021-01-01 20:51:13 +0700

    Day 04 Hola 2021 👋

---
+++ AdventOfCode2019/Day04/day04.py
@@ -0,0 +1,23 @@
+from re import finditer
+
+def password_count(input, p2):
+    r_min, r_max = [int(i) for i in input.split("-")]
+    return sum(1 for i in range(r_min, r_max+1) if check_valid(str(i), p2))
+    
+def check_valid(password, p2):
+    if password != "".join(sorted(list(password))):
+        return False
+    counts = [password.count(c) for c in set(password)]
+    if p2:
+        return any(count == 2 for count in counts)
+    else:
+        return any(count >= 2 for count in counts)
+    
+assert(check_valid("111111", False)) == True
+assert(check_valid("223450", False)) == False
+assert(check_valid("123789", False)) == False
+assert(check_valid("444444", True)) == False
+
+input = "402328-864247"
+print(password_count(input, False))
+print(password_count(input, True))
