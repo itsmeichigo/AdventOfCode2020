3533cc4960c9270dc623bc6c1d8579e33a5b1b82
Huong Do <hi.huongdo@gmail.com>
2021-12-01 14:15:25 +0700

    AoC 2021 Day 01

---
+++ AdventOfCode2021/Day01/day01.py
@@ -0,0 +1,27 @@
+def num_of_increases(depths):
+    count = 0
+    previous = None
+    for i in depths:
+        if previous and i > previous:
+            count += 1
+        previous = i
+    return count - 1
+
+def sliding_window_measurements(depths):
+    windows = []
+    current_sum = 0
+    current_index = 0
+    while current_index < len(depths):
+        current_sum += depths[current_index]
+        if current_index >= 2:
+            windows.append(current_sum)
+            current_sum -= depths[current_index - 2]
+        current_index += 1
+    return windows
+
+
+with open("data.txt") as file:
+    input = file.read()
+    depths = [int(i) for i in input.splitlines()]
+    print(num_of_increases(depths))
+    print(num_of_increases(sliding_window_measurements(depths)))
