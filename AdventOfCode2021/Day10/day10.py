244aaa4d2a82669ff0fadadc31140ea5bbb1b12b
Huong Do <hi.huongdo@gmail.com>
2021-12-11 18:07:08 +0700

    Part 2

--- AdventOfCode2021/Day10/day10.py
+++ AdventOfCode2021/Day10/day10.py
@@ -1,15 +1,17 @@
-from typing import List
+from typing import List, Tuple
 
 def parse_input(filename) -> List[str]:
     with open(filename) as file:
         return file.read().splitlines()
 
-def calculate_corrupted_lines(input) -> int:
-    points = 0
-    matching_brackets = {"]": "[", "}": "{", ")": "(", ">": "<"}
-    corrupted_points = {")": 3, "]": 57, "}": 1197, ">": 25137}
+def check_brackets(input) -> Tuple[int, int]:
+    corrupted_point, complete_points = 0, []
+    matching_brackets = {"]": "[", "[": "]", "}": "{", "{": "}", ")": "(", "(": ")", ">": "<", "<": ">"}
+    corrupted_map = {")": 3, "]": 57, "}": 1197, ">": 25137}
+    complete_map = {")": 1, "]": 2, "}": 3, ">": 4}
     for line in input:
         brackets = []
+        corrupted = False
         for char in line:
             if char in "[({<":
                 brackets.append(char)
@@ -17,13 +19,25 @@ def calculate_corrupted_lines(input) -> int:
                 if matching_brackets[char] == brackets[-1]:
                     brackets.pop(-1)
                 else:
-                    points += corrupted_points[char]
+                    corrupted_point += corrupted_map[char]
+                    corrupted = True
                     break
+        if corrupted: continue
+        score = 0
+        for i in range(len(brackets) - 1, -1, -1):
+            matching = matching_brackets[brackets[i]]
+            score *= 5
+            score += complete_map[matching]
+        complete_points.append(score)
 
-    return points
+    complete_points.sort()
+    middle_index = int(len(complete_points) / 2)
+    return (corrupted_point, complete_points[middle_index])
 
 test_input = parse_input("test.txt")
-assert calculate_corrupted_lines(test_input) == 26397
+assert check_brackets(test_input) == (26397, 288957)
 
 data = parse_input("data.txt")
-print("Part 1: ", calculate_corrupted_lines(data))
+result = check_brackets(data)
+print("Part 1: ", result[0])
+print("Part 2: ", result[1])
