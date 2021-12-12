f401e77a7be0d2dee4ffe54d02ab3b141de2d71d
Huong Do <hi.huongdo@gmail.com>
2021-12-12 20:31:54 +0700

    Part 2

--- AdventOfCode2021/Day12/day12.py
+++ AdventOfCode2021/Day12/day12.py
@@ -15,30 +15,34 @@ def parse_input(file_name):
                 subpaths[destiny] = [origin]
         return subpaths
 
-def count_paths(input) -> int:
+def count_paths(input, allows_visiting_twice) -> int:
     total = 0
     for cave in input["start"]:
-        total += find_paths(input, ["start", cave])
+        total += find_paths(input, ["start", cave], allows_visiting_twice)
     return total
 
-def find_paths(input, current_caves) -> int:
+def find_paths(input, current_caves, allows_visiting_twice) -> int:
     last_cave = current_caves[-1]
     total = 0
     for next_cave in input[last_cave]:
-        if next_cave.islower() and next_cave in current_caves:
-            continue
         if next_cave == "end":
             total += 1
             continue
-        copy = current_caves.copy()
-        copy.append(next_cave)
-        total += find_paths(input, copy)
+        if next_cave.isupper() or next_cave not in current_caves:
+            total += find_paths(input, current_caves + [next_cave], allows_visiting_twice)
+    if allows_visiting_twice:
+        for next_cave in input[last_cave]:
+            if next_cave.islower() and next_cave in current_caves:
+                total += find_paths(input, current_caves + [next_cave], False)
     return total
 
 tests = {"test1.txt": 10, "test2.txt": 19, "test3.txt": 226}
 for test, result in tests.items():
     input = parse_input(test)
-    assert(count_paths(input)) == result
+    assert(count_paths(input, False)) == result
+    if test == "test1.txt":
+        assert(count_paths(input, True)) == 36
 
 data = parse_input("data.txt")
-print("Part 1: ", count_paths(data))
+print("Part 1: ", count_paths(data, False))
+print("Part 2: ", count_paths(data, True))
