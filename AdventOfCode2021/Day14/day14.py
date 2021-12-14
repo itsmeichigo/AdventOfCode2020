ae54394672fac171b7c73098a14166d4ce905ad0
Huong Do <hi.huongdo@gmail.com>
2021-12-14 23:17:02 +0700

    Getting there

--- AdventOfCode2021/Day14/day14.py
+++ AdventOfCode2021/Day14/day14.py
@@ -4,41 +4,37 @@ def parse_input(file_name):
         rule_map = {}
         for rule in rules.splitlines():
             pair, element = rule.split(" -> ")
-            rule_map[pair] = pair[0] + element + pair[1]
+            rule_map[pair] = element
         return (template, rule_map)
 
-def execute_insertion(template, rules, step_count) -> str:
-    current = template
-    for _ in range(step_count):
-        new_groups = []
-        for i in range(len(current) - 1):
-            substring = current[i:i+2]
-            if substring in rules:
-                new_groups.append(rules[substring])
-        updated = ""
-        for group in new_groups:
-            if len(updated) == 0: updated = group
-            else:
-                updated = updated[:-1] + group
-        current = updated
-    return current
-
-def count_elements(input) -> int:
+def count_elements(template, rules, step_count) -> int:
     element_map = {}
-    for char in input:
+    for char in template:
         if char not in element_map:
             element_map[char] = 0
         element_map[char] += 1
+    pairs = []
+    for i in range(len(template) - 1):
+        pairs.append(template[i:i+2])
+    for pair in pairs:
+        count_after_insertion(pair, step_count, rules, element_map)
     counts = list(element_map.values())
     counts.sort()
     return counts[-1] - counts[0]
 
+def count_after_insertion(pair, step_count, rules, element_map):
+    if pair not in rules or step_count == 0:
+        return
+    inserted_char = rules[pair]
+    if inserted_char not in element_map:
+        element_map[inserted_char] = 0
+    element_map[inserted_char] += 1
+    count_after_insertion(pair[0] + inserted_char, step_count - 1, rules, element_map)
+    count_after_insertion(inserted_char + pair[1], step_count - 1, rules, element_map)
+
 test_template, test_rules = parse_input("test.txt")
-test_output_10 = execute_insertion(test_template, test_rules, 10)
-assert count_elements(test_output_10) == 1588
-test_output_40 = execute_insertion(test_template, test_rules, 40)
-assert count_elements(test_output_40) == 2188189693529
+assert count_elements(test_template, test_rules, 10) == 1588
+# assert count_elements(test_template, test_rules, 40) == 2188189693529
 
 template, rules = parse_input("data.txt")
-after_step_10 = execute_insertion(template, rules, 10)
-print("Part 1: ", count_elements(after_step_10))
+print("Part 1: ", count_elements(template, rules, 10))
