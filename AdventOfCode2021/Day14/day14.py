5eab722bdc0561cb42f478f18bed7034538b3325
Huong Do <hi.huongdo@gmail.com>
2021-12-15 09:15:12 +0700

    Count letters and pairs

--- AdventOfCode2021/Day14/day14.py
+++ AdventOfCode2021/Day14/day14.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def parse_input(file_name):
     with open(file_name) as file:
         template, rules = file.read().split("\n\n")
@@ -8,33 +10,34 @@ def parse_input(file_name):
         return (template, rule_map)
 
 def count_elements(template, rules, step_count) -> int:
-    element_map = {}
-    for char in template:
-        if char not in element_map:
-            element_map[char] = 0
-        element_map[char] += 1
-    pairs = []
+    pair_count, letter_count = Counter(), Counter()
     for i in range(len(template) - 1):
-        pairs.append(template[i:i+2])
-    for pair in pairs:
-        count_after_insertion(pair, step_count, rules, element_map)
-    counts = list(element_map.values())
-    counts.sort()
-    return counts[-1] - counts[0]
+        pair = template[i:i+2]
+        pair_count[pair] += 1
+    
+    for letter in template:
+        letter_count[letter] += 1
 
-def count_after_insertion(pair, step_count, rules, element_map):
-    if pair not in rules or step_count == 0:
-        return
-    inserted_char = rules[pair]
-    if inserted_char not in element_map:
-        element_map[inserted_char] = 0
-    element_map[inserted_char] += 1
-    count_after_insertion(pair[0] + inserted_char, step_count - 1, rules, element_map)
-    count_after_insertion(inserted_char + pair[1], step_count - 1, rules, element_map)
+    for _ in range(step_count):
+        updated = Counter()
+        for pair, count in pair_count.items():    
+            if pair not in rules: continue        
+            letter = rules[pair]
+            updated[pair[0] + letter] += count
+            updated[letter + pair[1]] += count
+            if letter in letter_count:
+                letter_count[letter] += count
+            else:
+                letter_count[letter] = 1
+        pair_count = updated
+
+    result = letter_count.most_common()[0][1] - letter_count.most_common()[-1][1]
+    return result
 
 test_template, test_rules = parse_input("test.txt")
 assert count_elements(test_template, test_rules, 10) == 1588
-# assert count_elements(test_template, test_rules, 40) == 2188189693529
+assert count_elements(test_template, test_rules, 40) == 2188189693529
 
 template, rules = parse_input("data.txt")
 print("Part 1: ", count_elements(template, rules, 10))
+print("Part 1: ", count_elements(template, rules, 40))
