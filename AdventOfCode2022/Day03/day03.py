04839ffa071167171cb6deb81e599b9d4a8af813
Huong Do <hi.huongdo@gmail.com>
2022-12-03 23:20:46 +0700

    Day 03 part 2

--- AdventOfCode2022/Day03/day03.py
+++ AdventOfCode2022/Day03/day03.py
@@ -1,16 +1,27 @@
 import string
 
-letters = list(string.ascii_lowercase)
-
-def calculate_priority(rucksack):
+def calculate_incorrect_item(rucksack):
     size = len(rucksack)//2
     first, second = set(rucksack[:size]), set(rucksack[size:])
     duplicate = list(first & second)[0]
-    index = letters.index(duplicate.lower())
-    return index + 1 if duplicate.islower() else index + 1 + len(letters)
+    return calculate_priority(duplicate)
+
+def calculate_group(group):
+    sets = [set(i) for i in group]
+    shared_item = list(sets[0] & sets[1] & sets[2])[0]
+    return calculate_priority(shared_item)
+
+def calculate_priority(item):
+    letters = list(string.ascii_lowercase)
+    index = letters.index(item.lower())
+    return index + 1 if item.islower() else index + 1 + len(letters)
 
 with open("input.txt") as file:
     lines = file.read().splitlines()
-    total_priorities = sum([calculate_priority(list(r)) for r in lines])
-    print(total_priorities)
-    
+    total_incorrect_item = sum(calculate_incorrect_item(list(r)) for r in lines)
+    print(total_incorrect_item)
+
+    total_groups = 0
+    for i in range(0, len(lines), 3):
+        total_groups += calculate_group(lines[i:i+3])
+    print(total_groups)    
