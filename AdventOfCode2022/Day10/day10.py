3e8a1ef654ac8dc70049efb960ca2a530204be27
Huong Do <hi.huongdo@gmail.com>
2022-12-11 18:23:20 +0700

    Part 2 day 10

--- AdventOfCode2022/Day10/day10.py
+++ AdventOfCode2022/Day10/day10.py
@@ -3,7 +3,10 @@ with open("input.txt") as file:
     current_value, pending_value = 1, None
     instruction_index, total = 0, 0
     cycles = [20, 60, 100, 140, 180, 220]
-    for cycle in range(1, 221):
+    pixels = [["." for i in range(40)] for j in range(6)]
+    for cycle in range(1, 241):
+        if (cycle-1)%40 in range(current_value-1, current_value+2):
+            pixels[(cycle - 1)//40][(cycle - 1)%40] = "#"
         if cycle in cycles:
             total += current_value * cycle
         if pending_value is not None:
@@ -16,3 +19,5 @@ with open("input.txt") as file:
             i = instructions[instruction_index]
             pending_value = int(i.split(" ")[1])
     print(total)
+    image = "\n".join(["".join(line) for line in pixels])
+    print(image)
